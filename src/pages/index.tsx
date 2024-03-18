d1cd4483013e0f4ea2b89fa7820089bd08acfb4e
yan <yanhaibiao1991@gmail.com>
2024-03-18 12:36:37 +0800

    msafe test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { MSafeWallet } from "@msafe/sui-wallet";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  console.log(MSafeWallet);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
